f3ea7b1c83b8de312a59e7c00d3a73a67da89dc6
Robert Battaglia <r083rt164@gmail.com>
2023-05-18 13:49:55 -0400

    custom telescope to ensure hidden files found

---
+++ lua/custom/plugins/telescope.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		'nvim-telescope/telescope.nvim',
+		branch = '0.1.x',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+		config = function()
+			require('telescope').setup({
+				pickers = {
+					find_files = {
+						find_command = { 'rg', '--files', '--hidden', '-g', '!.git' }
+					}
+				}
+
+			})
+		end
+	},
+}
