aa9e44a736e45c9c145224319037800d112c2ad7
Robert Battaglia <r083rt164@gmail.com>
2023-09-26 15:06:42 -0400

    add autopairs and autotags plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -31,5 +31,33 @@ return {
 			"nvim-lua/plenary.nvim",
 			"nvim-telescope/telescope.nvim"
 		}
+	},
+	{
+		"sourcegraph/sg.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+
+		-- If you have a recent version of lazy.nvim, you don't need to add this!
+		build = "nvim -l build/init.lua",
+	},
+	{
+		"windwp/nvim-autopairs",
+		dependencies = { 'hrsh7th/nvim-cmp' },
+		config = function()
+			require("nvim-autopairs").setup {}
+			-- If you want to automatically add `(` after selecting a function or method
+			local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+			local cmp = require('cmp')
+			cmp.event:on(
+				'confirm_done',
+				cmp_autopairs.on_confirm_done()
+			)
+		end,
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		dependencies = { 'nvim-treesitter/nvim-treesitter' },
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end
 	}
 }
