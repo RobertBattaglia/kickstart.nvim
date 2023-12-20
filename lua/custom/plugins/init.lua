7c0b9d6fa45dac8ea9bc3202a2ff02d9d427fe5b
Robert Battaglia <r083rt164@gmail.com>
2023-12-20 12:26:46 -0500

    add keymap to delete buffers within telescope

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -35,9 +35,6 @@ return {
 	{
 		"sourcegraph/sg.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
-
-		-- If you have a recent version of lazy.nvim, you don't need to add this!
-		build = "nvim -l build/init.lua",
 	},
 	{
 		"windwp/nvim-autopairs",
--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -8,6 +8,13 @@ return {
 				pickers = {
 					find_files = {
 						find_command = { 'rg', '--files', '--hidden', '-g', '!.git' }
+					},
+					buffers = {
+						mappings = {
+							i = {
+								["<c-d>"] = "delete_buffer"
+							}
+						}
 					}
 				}
 
