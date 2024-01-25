320a356359d23fb2894e75293b8fca93e546a6dd
Robert Battaglia <r083rt164@gmail.com>
2024-01-25 13:02:40 -0500

    fix cody issue

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -35,6 +35,9 @@ return {
 	{
 		"sourcegraph/sg.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require('sg').setup({})
+		end,
 	},
 	{
 		"windwp/nvim-autopairs",
